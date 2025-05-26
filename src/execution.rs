9d8188445b2d03f13030bb6ea11778cc60b77900
Jonstep101010 <git@jschwabe.site>
2025-05-26 14:31:15 +0200

    Block 3: basic cd, exit, pwd
    - fix clippy
    
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- src/execution.rs
+++ src/execution.rs
@@ -1,5 +1,8 @@
 pub use crate::parsing::{Cmd, Element};
-use std::process::Command;
+use std::{
+	path::PathBuf,
+	process::{Command, Output},
+};
 
 pub trait ElementVec {
 	// Add your methods here
@@ -35,17 +38,76 @@ impl ElementVec for Vec<Element> {
 	}
 }
 
+mod builtins {
+	use std::{path::PathBuf, process::Output};
+
+	pub struct Cd {
+		dir: PathBuf,
+	}
+
+	impl Cd {
+		pub fn new(dir: PathBuf) -> Self {
+			Self { dir }
+		}
+		pub fn run(self) -> Result<Option<Output>, std::io::Error> {
+			if self.dir.exists() {
+				// go there
+				std::env::set_current_dir(self.dir)?;
+				Ok(None)
+			} else {
+				Err(std::io::ErrorKind::NotADirectory.into())
+			}
+		}
+	}
+	pub struct Exit {
+		status: i32,
+	}
+
+	impl Exit {
+		pub fn new(status: i32) -> Self {
+			Self { status }
+		}
+		pub fn run(self) -> Result<Option<Output>, std::io::Error> {
+			std::process::exit(self.status);
+		}
+	}
+}
+
 // get single input from stdin
 // run single command
 impl Cmd {
 	// replaced by Parser: from_line (single cmd)
 	/// runs command in separate process
 	pub fn run(self) -> Option<std::process::Output> {
-		let process = Command::new(self.binary)
-			.args(self.args)
-			.spawn()
-			.map_err(|e| eprintln!("{e:?}"))
-			.ok()?;
-		process.wait_with_output().ok()
+		// set up args for builtins
+		let result = match self.binary.as_ref() {
+			"cd" => {
+				let dir = self.args.first()?;
+				let dir_pbuf = PathBuf::from(dir);
+				builtins::Cd::new(dir_pbuf).run()
+			}
+			"pwd" => {
+				let path = std::env::current_dir().expect("cwd should exist!");
+				println!("{}", path.display());
+				Ok(None)
+			}
+			"exit" => {
+				let status = match self.args.first() {
+					Some(status) => status.parse().unwrap_or_default(),
+					None => 0,
+				};
+				builtins::Exit::new(status).run()
+			}
+			_ => self.run_external(),
+		};
+		if let Err(e) = result {
+			eprintln!("{e:?}");
+		};
+		None
+	}
+	pub fn run_external(self) -> Result<Option<Output>, std::io::Error> {
+		let process = Command::new(self.binary).args(self.args).spawn()?;
+		let output = process.wait_with_output()?;
+		Ok(Some(output))
 	}
 }
