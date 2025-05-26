99e5cf02f323df42f14bee5b12b94f347625bb8f
Jonstep101010 <git@jschwabe.site>
2025-05-26 10:52:05 +0200

    Block 2: implement template variation
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,129 @@ struct Cmd {
 	args: Vec<String>,
 }
 
+#[derive(PartialEq, Debug)]
+enum Element {
+	/// `&&`
+	And,
+	/// `||`
+	Or,
+	/// Command.
+	#[allow(clippy::enum_variant_names)]
+	ElementCmd(Cmd),
+}
+
+trait ElementVec {
+	// Add your methods here
+	fn run(self);
+}
+
+impl ElementVec for Vec<Element> {
+	// Implement your methods here
+	fn run(self) {
+		use Element::ElementCmd;
+		let mut previous_output = None;
+		for elem in self {
+			match elem {
+				ElementCmd(cmd) => {
+					previous_output = cmd.run();
+				}
+				Element::And => {
+					let status = previous_output.expect("no command before &&").status;
+					if !status.success() {
+						break;
+					}
+					previous_output = None;
+				}
+				Element::Or => {
+					let status = previous_output.expect("no command before ||").status;
+					if !status.success() {
+						break;
+					}
+					previous_output = None;
+				}
+			}
+		}
+	}
+}
+
+/// processes input: `chain_in_line` (already split by ';')
+/// Parse `[Element]`s from a string.
+#[derive(Debug)]
+struct Parser {
+	current: usize,
+	tokens: Vec<String>,
+}
+
+impl Parser {
+	///
+	/// `chain_in_line`: single input to split by whitespace
+	fn new(chain_in_line: &str) -> Self {
+		dbg!(Self {
+			tokens: chain_in_line.split_whitespace().map(String::from).collect(),
+			current: 0,
+		})
+	}
+	fn parse_cmd(&mut self, binary: String) -> Option<Cmd> {
+		let mut args: Vec<String> = vec![];
+		loop {
+			let next = self.tokens.get(self.current);
+			match next {
+				Some(token) if token == "&&" => break,
+				Some(token) if token == "||" => break,
+				Some(token) => {
+					args.push(token.to_string());
+				}
+				None => break,
+			}
+			self.current += 1;
+		}
+		Some(dbg!(Cmd { binary, args }))
+	}
+	fn parse(mut self) -> Option<Vec<Element>> {
+		let mut elements = vec![];
+		while let Some(elem) = self
+			.tokens
+			.get(self.current)
+			.map(|s| s.to_string())
+			.and_then(|next| {
+				self.current += 1;
+				match next.as_str() {
+					"&&" => Some(Element::And),
+					"||" => Some(Element::Or),
+					_ => Self::parse_cmd(&mut self, next.to_string()).map(Element::ElementCmd),
+				}
+			}) {
+			elements.push(elem);
+		}
+		// handle empty
+		if elements.is_empty() {
+			None
+		} else {
+			Some(elements)
+		}
+	}
+}
+
 // get single input from stdin
 // run single command
 impl Cmd {
-	fn from_line(line: &str) -> Option<Self> {
-		dbg!(&line);
-		let mut parts = line.split_whitespace().map(String::from);
-		parts.next().map(|binary| {
-			dbg!(Cmd {
-				binary,
-				args: parts.collect(),
-			})
-		})
-	}
-	///
+	// replaced by Parser: from_line (single cmd)
 	/// runs command in separate process
-	pub fn run(self) {
-		let process = Command::new(self.binary).args(self.args).spawn();
-		match process {
-			Ok(mut child) => {
-				child.wait().expect("command wasn't running");
-			}
-			Err(e) => eprintln!("{e:?}"),
-		}
+	pub fn run(self) -> Option<std::process::Output> {
+		let process = Command::new(self.binary)
+			.args(self.args)
+			.spawn()
+			.map_err(|e| eprintln!("{e:?}"))
+			.ok()?;
+		process.wait_with_output().ok()
 	}
+}
+
+fn parse_multiple(line: &str) -> Vec<Vec<Element>> {
+	// inefficient: parses whole line with split (instead of char by char)
+	line.split(';')
+		.filter_map(|s| Parser::new(s).parse())
+		.collect()
 }
 
 fn main() -> io::Result<()> {
@@ -50,7 +149,7 @@ fn main() -> io::Result<()> {
 		let mut input_line = String::new();
 		std::io::stdin().read_line(&mut input_line)?;
 		// Parse line into executable command
-		if let Some(cmd) = Cmd::from_line(&input_line) {
+		for cmd in parse_multiple(&input_line) {
 			// Execute the command in a separate process
 			cmd.run()
 		}
@@ -61,31 +160,48 @@ fn main() -> io::Result<()> {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use Element::*;
 
 	#[test]
 	fn no_cmd_is_parsed_from_empty_line() {
-		assert_eq!(Cmd::from_line(""), None);
+		assert_eq!(parse_multiple(""), Vec::<Vec<Element>>::new());
 	}
 
 	#[test]
 	fn cmd_with_no_args_is_parsed() {
 		assert_eq!(
-			Cmd::from_line("ls"),
-			Some(Cmd {
+			parse_multiple("ls"),
+			vec![vec![ElementCmd(Cmd {
 				binary: "ls".to_string(),
 				args: vec![]
-			})
+			}),],]
 		);
 	}
 
 	#[test]
 	fn cmd_with_args_is_parsed() {
 		assert_eq!(
-			Cmd::from_line("ls -l"),
-			Some(Cmd {
+			parse_multiple("ls -l"),
+			vec![vec![ElementCmd(Cmd {
 				binary: "ls".to_string(),
 				args: vec!["-l".to_string()]
-			})
+			})]]
 		);
+	}
+	#[test]
+	fn cmds_are_parsed() {
+		assert_eq!(
+			parse_multiple("ls; echo hello"),
+			[
+				[ElementCmd(Cmd {
+					binary: "ls".to_string(),
+					args: vec![]
+				})],
+				[ElementCmd(Cmd {
+					binary: "echo".to_string(),
+					args: vec!["hello".to_string()]
+				})]
+			]
+		)
 	}
 }
